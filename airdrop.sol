cb282e7a08c59eda5064ee4b3fc1376dacb6c7a5
Oktest <35876354+aicgi@users.noreply.github.com>
2023-11-14 11:05:48 +0700

    Update airdrop.sol

--- airdrop.sol
+++ airdrop.sol
@@ -164,15 +164,17 @@ contract Airdrop is ReentrancyGuard, Context, Ownable{
         emit WhitelistSetted(recipients, amount);
     }
 
-    function claimTokens() public nonReentrant {
+    function claimTokensWithFee() public payable {
         require(airdropLive == true, 'Airdrop not started yet');
-        require(Claimed[msg.sender] == false, 'Airdrop already claimed!');
-        if(_token.balanceOf(address(this)) == 0) { airdropLive = false; return;}
-        Claimed[msg.sender] = true;
+        require(Claimed[msg.sender] == false, 'Airdrop already claimed!')
+        uint256 claimFee = 0.000989 BNB;
+        require(msg.value >= claimFee, 'Insufficient fee provided');
         uint256 amount = _valDrop[msg.sender].mul(10**9);
         _token.transfer(msg.sender, amount);
+        payable(address(this)).transfer(msg.value);
+        Claimed[msg.sender] = true;
         emit AirdropClaimed(msg.sender, amount);
-    }
+   }
     
     function withdraw() external onlyOwner {
          require(address(this).balance > 0, 'Contract has no money');
